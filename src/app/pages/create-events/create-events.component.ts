b4105a2e4e3c5f1adfec5f1b896e11088e5e0dd3
TaniaSanchezM <tania.sm90@gmail.com>
2021-11-30 15:11:03 +0100

    Página Crear Evento

--- src/app/pages/create-events/create-events.component.ts
+++ src/app/pages/create-events/create-events.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-events.component.css']
 })
 export class CreateEventsComponent implements OnInit {
-
+  public evento =[]
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  createEvent(img:string, titulo:string, deporte:string, fecha:string, hora:string, ubicacion:string, personas:string, material:string, pago:string, edad:string){
+    // if (material === "option1")
+    this.evento = [img,titulo, deporte, fecha, hora, ubicacion, personas, material, pago, edad]
+    console.log(this.evento);
+    
+  }
+  
 }
+
