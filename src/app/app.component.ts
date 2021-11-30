e4d8fe267fd862e073c1c29279f053ef45cb4b86
Daniel Aramburu <aramburu.daniel.galeano@gmail.com>
2021-11-30 15:47:49 +0100

    Se ha ocultado el header y el footer en las paginas de login, register y recover-password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'irateams';
+
+  constructor(public routeLocation: Location){}
 }
