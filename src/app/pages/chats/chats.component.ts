3fef30212c4dcaee8fecedcecb7527fdacc824a0
raulcasas <rules37@hotmail.com>
2021-12-11 21:50:08 +0100

    página de todos los mensajes

--- src/app/pages/chats/chats.component.ts
+++ src/app/pages/chats/chats.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ChatService } from '../../shared/chat.service';
+import { Chat } from '../../models/chat';
+import { LoginService } from '../../shared/login.service';
+import { Mensaje } from '../../models/mensaje';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chats',
@@ -7,8 +13,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatsComponent implements OnInit {
 
-  constructor() { }
-
+  public chats: any;
+  public numchat:number
+  constructor(private chatService: ChatService, private loginService: LoginService, private router: Router) 
+  {
+    this.chats = []
+    this.chatService.getChats(this.loginService.login.userId).subscribe((data:any) => 
+    {
+      this.chats = data.resultado
+    })
+  }
+  public showMSG(id:number){
+    this.numchat =  id
+    this.router.navigate(["/chat-id"])
+  }
   ngOnInit(): void {
   }
 
