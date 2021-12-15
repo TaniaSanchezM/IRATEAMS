3c8780335dffa96bc7a06526cf0ccd8ec5a21eeb
TaniaSanchezM <tania.sm90@gmail.com>
2021-12-15 11:49:40 +0100

    Subida mis creados

--- src/app/pages/my-created-events/my-created-events.component.ts
+++ src/app/pages/my-created-events/my-created-events.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MyCreatedService } from '../../shared/my-created.service';
+import { Event } from 'src/app/models/events';
+import { LoginService } from 'src/app/shared/login.service';
+import { EventosService } from 'src/app/shared/eventos.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-my-created-events',
@@ -6,16 +11,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-created-events.component.css']
 })
 export class MyCreatedEventsComponent implements OnInit {
-  public siClick : boolean = false
-  constructor() { 
+  public siClick : boolean = false;
+  public misCreados: Event[];
+  public eventSelected: Event
+  public id_eventoSelect:number
+  public borrado: Event
+
+  constructor(public misCreadosService:MyCreatedService, public loginService:LoginService, public homeService: EventosService, private toastr: ToastrService) { 
   }
   editor(){
-    this.siClick = true
-
-  
+    this.siClick = true  
   }
 
+  showMyCreated(){
+    let id_usuario = this.loginService.login.userId;
+    this.misCreadosService.getMisCreados(id_usuario).subscribe((data:any)=>
+    {
+      this.misCreados = data.resultado
+      console.log(this.misCreados);      
+    })
+  }
+  getIdEvento(id: number)
+  {
+    console.log(id)
+    this.homeService.eventoId = id;
+    console.log(this.homeService.eventoId)
+  }
+
+  saveId(id_evento:number){
+
+    this.id_eventoSelect = id_evento
+    console.log(this.id_eventoSelect)
+  }
+  deleteEvento(){
+    console.log(this.id_eventoSelect);
+    
+    this.misCreadosService.deleteMiCreado(this.id_eventoSelect).subscribe((data:any)=>
+    {
+      this.borrado = data
+      console.log(this.borrado);
+    })
+    this.showSuccess()
+    this.showMyCreated()
+  }
+
+  showSuccess() {
+    this.toastr.success('', 'Perfil actualizado',{timeOut:4000, positionClass:"toast-top-full-width"});
+  }
+  showError(){
+    this.toastr.error('', 'Error al actualizar el perfil',{timeOut:4000, positionClass:"toast-top-full-width"});
+  }
   ngOnInit(): void {
+    this.showMyCreated()
   }
 
 }
