af4d07d8a3a4a625e583af8a1d2be37164f5bc85
Daniel Aramburu <aramburu.daniel.galeano@gmail.com>
2021-12-19 22:48:12 +0100

    Se ha quitado el mantener sesion mediante sessionStorage

--- src/app/models/login.ts
+++ src/app/models/login.ts
@@ -1,19 +1,19 @@
 export class Login {
-    private _userId:        number;
+    public userId:        number;
     public userCredentials: string;
     public password:        string;
     constructor(){
         
     }
-    get userId() {
-        return this._userId || Number(sessionStorage.getItem('userId'));
-    }
-    set userId(value: number) {
-        if (value) {
-            sessionStorage.setItem('userId', String(value))
-        } else {
-            sessionStorage.removeItem('userId');
-        }
-        this._userId = value;
-    }
+    // get userId() {
+    //     return this._userId || Number(sessionStorage.getItem('userId'));
+    // }
+    // set userId(value: number) {
+    //     if (value) {
+    //         sessionStorage.setItem('userId', String(value))
+    //     } else {
+    //         sessionStorage.removeItem('userId');
+    //     }
+    //     this._userId = value;
+    // }
 }
